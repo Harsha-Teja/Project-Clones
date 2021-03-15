6ef94b0ea5f7929edbedf52cfc7e000bb649520a
Harsha-Teja <harshatejanageshwaran@gmail.com>
2021-03-16 00:59:56 +0530

    staticwebsite

--- staticwebsite/src/App.js
+++ staticwebsite/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Vacancies from "./vacancies.json"
 import {useState} from "react"
 import logo from "./school.svg"
+import teacherrlogo from "./logo.png"
 
 function App() {
 
@@ -10,20 +11,25 @@ function App() {
 
   return (
     <div className="app">
-          <h1 className="maintitle">Teacherr Job Mela</h1>
-          <h5 className="subtitle">Hello Teachers, here you can find the list of vacancies for the job mela. </h5>
-          <p className="description">Please, enter your name, year of experience and subject you teach.
-              You will find the list of vacancies that you can apply by clicking on them.
+          <img src={teacherrlogo} alt="teacherr logo" className="mainlogo"/>
+          <h1 className="maintitle">Welcome to Teacherr Job Mela 2021!!</h1>
+          <h5 className="subtitle">Teacherr Job Mela is an online job fair created for teachers looking  for high paying teaching jobs.  . </h5>
+          <p className="description">Find your dream teaching job in 2 steps! </p>
+          <p className="description">
+                1) Enter the subject and division that you teach.
+                2) Discover teaching jobs and click on the "Upload Resume Here" to submit your video resume.
           </p>
           <div className="form">
             <input
                         type="text"
                         name="subject"
                         id="subject"
-                        placeholder="Search for a subject..."
+                        placeholder="Search for a JobRole..."
                         className="formsubject"
                         onChange={event => {setSearchSub(event.target.value)}}
             />
+            </div>
+            <div className="form">
             <select
                         id="division"
                         name="division"
@@ -41,21 +47,24 @@ function App() {
           </div>
           <div className="container">
                 {Vacancies.Vacancies.filter((data) => {
-                  if (searchdiv === '' || setSearchDiv === ''){
-                    return data
-                  } else if ( data.subject.toLocaleLowerCase().includes(searchsub.toLocaleLowerCase()) && 
-                              data.division.toLocaleLowerCase().includes(searchdiv.toLocaleLowerCase())
-                  ){
+                  if (searchdiv === '' || searchsub === ''){
                     return data
                   }
-                  
+                  else if ((data.division !== "") && searchsub === ''
+                  ){
+                    return data
+                  } 
+                  else if ( data.jobrole.toLocaleLowerCase().includes(searchsub.toLocaleLowerCase()) && 
+                              (data.division === searchdiv)
+                  ){
+                    return data
+                  } 
                 }).map((data) => {
                   return (
                     <ul className="details" key={data.id}> 
                     <div className="detailssplit">
                         <div className="detailssplitdata">
-                          <li className="detailtitlelist"><span className="detailtitle">School Name:</span> {data.schoolname}</li>
-                          <li className="detailtitlelist"><span className="detailtitle">Subject:</span> {data.subject}</li>
+                          <li className="detailtitlelist"><span className="detailtitle">Institute Name:</span> {data.schoolname}</li>
                           <li className="detailtitlelist"><span className="detailtitle">Job Role:</span> {data.jobrole}</li>
                           <li className="detailtitlelist"><span className="detailtitle">Division:</span> {data.division}</li>
                           <li className="detailtitlelist"><span className="detailtitle">Location:</span> {data.location}</li>
@@ -64,7 +73,7 @@ function App() {
                           <img src={logo} alt="school logos" className="detailsimg"/>
                         </div>
                     </div>
-                      <a href={data.flipgridlink} className="link"><li className="detailtitlelistlink">Upload Resume Here</li></a>  
+                      <a href={data.flipgridlink} className="link" target="_blank" rel="noreferrer"><li className="detailtitlelistlink">Upload Resume Here</li></a>  
                     </ul>
                   )
                 })}
